22132d3352eeed6aa3a4c5297c133dbd18c72f13
Rois Admaja <43290237+Admaja-is-Admj@users.noreply.github.com>
2019-02-20 17:25:37 +0700

    1.1

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -7,11 +7,18 @@ import { CoursesService } from './courses.service';
   styleUrls: ['./courses.component.css']
 })
 export class CoursesComponent implements OnInit {
-  title = 'Angular';
-  point = '3 Author';
+  title = 'Belajar Angular';
   Courses;
-  constructor(private service:CoursesService) { 
-    this.Courses = service.getCourses();
+  binding = 'property-binding';
+  imageUrl = 'http://lorempixel.com/400/200';
+  colSpan = 2;
+  isActive = false;
+  // title = 'Angular';
+  // point = '3 Author';
+  // Courses;
+  constructor(private service:CoursesService) {
+    this.Courses = service.getCourses(); 
+  //   this.Courses = service.getCourses();
   }
   ngOnInit() {
   }
